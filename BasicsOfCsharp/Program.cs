8b1cd96dfc6d9837a9a683cc63830d3fa343a4c7
Santoshi1234 <vsantoshi231@gmail.com>
2022-04-29 12:57:14 +0530

    Add project files.

---
+++ BasicsOfCsharp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BasicsOfCsharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
